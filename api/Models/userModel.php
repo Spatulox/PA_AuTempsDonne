ef571a671e673c63f42c380c73a4b62eb6ed4f71
Spatulox <M78stormtrooper@laposte.net>
2024-02-18 15:58:07 +0100

    add userModel

---
+++ api/Models/userModel.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once './Repository/BDD.php';
+
+class UserModel {
+    public $id_user;
+    public $nom;
+    public $prenom;
+    public $email;
+    public $telephone;
+    public $date_inscription;
+    public $type;
+    public $role;
+    public $apikey;
+
+    public function __construct($id_user, $nom, $prenom, $date_inscription, $email = null, $telephone = null, $type = null, $role = null, $apikey = null) {
+        $this->id_user = $id_user;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->telephone = $telephone;
+        $this->date_inscription = $date_inscription;
+        $this->type = $type;
+        $this->role = $role;
+        $this->apikey = $apikey;
+    }
+}
+
+?>
