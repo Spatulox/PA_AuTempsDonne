aff2161d36f8effaea0a740bda2679ceac6e5d1b
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-02-22 01:05:29 +0100

    Update BeneficiairerRepository add function create activite

---
+++ api/Repository/beneficiairerRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once './Repository/BDD.php';
+include_once './Models/ActiviteModel.php';
+
+class BeneficiaireRepository {
+    //private $connection = null;
+
+    public function __construct() {
+        // Initialiser la connexion à la base de données
+        $this->connection = new BDD();
+    }
+
+    //-------------------------------------
+    public function createActivity(ActiviteModel $activiteModel) {
+        $activite = insertDB(
+            "ACTIVITES",
+            ["nom_activite", "type_activite", "date_activite", "index_activite"],
+            [$activiteModel->nom_activite, $activiteModel->type_activite, $activiteModel->date_activite, 2]
+        );
+
+        if(!$activite){
+            exit_with_message("Error, failed to create the activity, please try again", 403);
+        }
+
+        $activite = selectDB('ACTIVITES', '*', 'nom_activite="'.$activiteModel->nom_activite.'"');
+
+        return new ActiviteModel(
+            $activite[0]['id_activite'],
+            $activite[0]['nom_activite'],
+            $activite[0]['type_activite'],
+            $activite[0]['date_activite'],
+            $activite[0]['index_activite']);
+    }
+	//-------------------------------------
+
+}
+?>
