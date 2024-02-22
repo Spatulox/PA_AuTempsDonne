842610f934707662fb4b431feb33fb9dc5ae64ec
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-02-22 01:03:27 +0100

    update beneficiaireservice add fonction createactiviter

--- api/Models/activiteModel.php
+++ api/Models/activiteModel.php
@@ -3,15 +3,15 @@ include_once './Repository/BDD.php';
 
 class ActiviteModel {
     public $id_activite;
-    public $nom;
-    public $description;
+    public $nom_activite;
+    public $type_activite;
     public $date_activite;
     public $index_activite; 
 
-    public function __construct($id_activite, $nom, $description, $date_activite, $index_activite) {
+    public function __construct($id_activite, $nom_activite, $date_activite, $index_activite) {
         $this->id_activite = $id_activite;
-        $this->nom = $nom;
-        $this->description = $description;
+        $this->nom_activite = $nom_activite;
+        $this->type_activite = $type_activite;
         $this->date_activite = $date_activite;
         $this->index_activite = $index_activite;
     }
---
+++ api/Service/beneficiaireService.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once './Repository/BeneficiaireRepository.php'; 
+include_once './Models/ActiviteModel.php';
+
+class BeneficiaireService {
+    
+    public $beneficiaireRepository;
+
+    /*
+     *  dépendance pour le repository
+    */
+    public function __construct() {
+        $this->beneficiaireRepository = new BeneficiaireRepository();
+    }
+
+
+    /*
+     *  cree une activité
+    */
+    public function createActivite(ActiviteModel $activiteModel) {
+        return $this->beneficiaireRepository->createActivite($activiteModel);
+    }
+
+    
+}
+
+?>
