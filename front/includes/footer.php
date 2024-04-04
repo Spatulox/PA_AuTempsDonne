ff1762e7d8c8e5243f5b06ba064ecadda0d154c6
Spatulox <M78stormtrooper@laposte.net>
2024-04-04 16:25:23 +0200

    fix footer

--- front/includes/footer.php
+++ front/includes/footer.php
@@ -1,15 +1,7 @@
-<?php
-
-$file = file_get_contents('../lang/lang_fr.json');
-$data = json_decode($file, true);
-
-?>
-
 <footer>
 	<nav>
 		<ul>
 			<?php
-
 				foreach ($data["footer"]["li"] as $key => $value) {
 					echo('<li><a href="' . $key . '">' . $value . '</a></li>');
 				}
