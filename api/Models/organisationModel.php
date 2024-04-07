8af0bde2331064c4baef14a53e6372468eb6cbcf
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-04-07 14:49:44 +0200

    Create model planning

---
+++ api/Models/organisationModel.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once './Repository/BDD.php';
+
+class PlanningModel {
+    public $id_planning;
+    public $description;
+    public $lieux;
+    public $date_activite;
+    public $id_index;
+    public $id_activite;
+
+    public function __construct($id_user, $description, $lieux, $date_activite, $id_index, $id_activite) {
+        $this->id_user = $id_user;
+        $this->description = $description;
+        $this->lieux = $lieux;
+        $this->date_activite = $date_activite;
+        $this->id_index = $id_index;
+        $this->id_activite = $id_activite;
+    }
+
+}
+
+?>
