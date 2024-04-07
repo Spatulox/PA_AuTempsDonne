17e38f17d15765928ee2fda667c512d167f78064
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-04-07 14:50:11 +0200

    Update model add activiter

--- api/Models/organisationModel.php
+++ api/Models/organisationModel.php
@@ -19,6 +19,18 @@ class PlanningModel {
         $this->id_activite = $id_activite;
     }
 
+
+class ActiviteModel {
+    public $id_activite;
+    public $nom_activite;
+
+    public function __construct($id_activite, $nom_activite) {
+        $this->id_activite = $id_activite;
+        $this->nom_activite = $nom_activite;
+    }
+
+}
+
 }
 
 ?>
