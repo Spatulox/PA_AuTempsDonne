ca3a4b3ea9d846be3a1efac75193657c0575d3bd
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-04-07 15:36:35 +0200

    Upadte Planningcontroller POST

--- api/Repository/planningRepository.php
+++ api/Repository/planningRepository.php
@@ -46,7 +46,74 @@ class PlanningRepository {
         );
     }
 
+    //-------------------------------------
+    
+    public function createPlanning(PlanningModel $planning){
+        $id_planning = insertDB("PLANNING", ["id_planning", "description", "lieux", "date_activite", "id_index", "id_activite"], [
+            $planning->id_planning,
+            $planning->description,
+            $planning->lieux,
+            $planning->date_activite,
+            $planning->id_index,
+            $planning->id_activite
+        ]);
 
+        if(!$id_planning){
+            exit_with_message("Error, the planning can't be created, plz try again", 500);
+        }
+
+        $planning->setId($id_planning);
+        return $planning;
+    }
+
+    //-------------------------------------
+    
+    public function updatePlanning(PlanningModel $planning){
+        $id_planning = updateDB("PLANNING", ["id_planning", "description", "lieux", "date_activite", "id_index", "id_activite"], [
+            $planning->id_planning,
+            $planning->description,
+            $planning->lieux,
+            $planning->date_activite,
+            $planning->id_index,
+            $planning->id_activite
+        ], "id_planning=".$planning->getId());
+
+        if(!$id_planning){
+            exit_with_message("Error, the planning can't be updated, plz try again", 500);
+        }
+
+        return $planning;
+    }
+
+    //-------------------------------------
+
+    public function deletePlanning($id){
+        $deleted = deleteDB("PLANNING", "id_planning=".$id);
+
+        if(!$deleted){
+            exit_with_message("Error, the planning can't be deleted, plz try again", 500);
+        }
+
+        return true;
+    }
+
+     //-------------------------------------
+
+
+    public function joinActivity($userId, $planningId) {
+    // a faire verifie que l'utilisateur et l'activité existent avant d'effectuer l'opération.
+
+    $query = "INSERT INTO PARTICIPE (id_user, id_planning) VALUES (?, ?)";
+    $statement = $this->connection->prepare($query);
+    $success = $statement->execute([$userId, $planningId]);
+
+    //a faire verifier si l'opération d'insertion a réussi
+    if ($success) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 }
 ?>
