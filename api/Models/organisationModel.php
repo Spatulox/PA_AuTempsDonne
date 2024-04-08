4ac518360625556c1c305f8c4d7421ac6035aea8
Spatulox <M78stormtrooper@laposte.net>
2024-04-08 15:17:41 +0200

    fix organisationModel.php to match DB

--- api/Models/organisationModel.php
+++ api/Models/organisationModel.php
@@ -9,14 +9,23 @@ class PlanningModel {
     public $date_activite;
     public $id_index;
     public $id_activite;
+    public $activity_desc;
 
     public function __construct($id_user, $description, $lieux, $date_activite, $id_index, $id_activite) {
-        $this->id_user = $id_user;
+        $this->id_planning = $id_planning;
         $this->description = $description;
         $this->lieux = $lieux;
         $this->date_activite = $date_activite;
         $this->id_index = $id_index;
         $this->id_activite = $id_activite;
+    }
+
+    public function setId($id){
+        $this->id_planning = $id;
+    }
+
+    public function setActivity($activity){
+        $this->activity_desc = $activity[0];
     }
 }
 
