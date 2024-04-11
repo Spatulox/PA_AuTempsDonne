1a1d920cbd4dd1df90b7d26d7cfe84f9ce4662b5
Spatulox <M78stormtrooper@laposte.net>
2024-04-11 18:10:33 +0200

    add message system via url

--- front/HTML/index.php
+++ front/HTML/index.php
@@ -1,5 +1,20 @@
 <?php
-header('Location: actualites.php');
+
+
+function hasMessageInUrl($url) {
+    // Vérifier si l'URL contient "?body=" ou "&body="
+    if (strpos($url, '?message=') !== false || strpos($url, '&message=') !== false) {
+
+    	$message = explode("?message=", $url)[1];
+        return "?message=".$message;
+    } else {
+        return null;
+    }
+}
+
+$message = hasMessageInUrl($_SERVER['REQUEST_URI']);
+
+header('Location: actualites.php'.$message);
 exit()
 ?>
 
--- front/includes/footer.php
+++ front/includes/footer.php
@@ -1,3 +1,17 @@
+<?php
+function hasMessageInUrl($url) {
+    if (strpos($url, '?message=') !== false || strpos($url, '&message=') !== false) {
+
+    	$message = explode("?message=", $url)[1];
+        return "?message=".$message;
+    } else {
+        return null;
+    }
+}
+
+$message = hasMessageInUrl($_SERVER['REQUEST_URI']);
+?>
+
 <footer>
 	<nav>
 		<ul>
@@ -8,4 +22,27 @@
 			?>
 		</ul>
 	</nav>
+	<?php
+		if($message){
+			$message = explode("?message=", $message)[1];
+			$message = str_replace("%20", " ", $message);
+
+			echo('	<h2 class="" id="titleFooter" style="position:absolute;">
+						'.$message.'
+					</h2>
+				');
+		}
+	?>
+	
 </footer>
+
+<script type="text/javascript" defer>
+	let message
+
+	message = document.getElementById('titleFooter')
+
+	if(message){
+		popup(message.innerHTML)
+	}
+
+</script>
