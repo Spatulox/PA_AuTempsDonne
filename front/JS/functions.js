82b1e888b63956f7e04cd83d0cf73996692543b1
Spatulox <M78stormtrooper@laposte.net>
2024-04-17 00:43:56 +0200

    add connexion function to function.js

--- front/JS/functions.js
+++ front/JS/functions.js
@@ -1,8 +1,31 @@
+// functions.js
 function redirect(page, message = null) {
-    if(message){
-        window.location.href = page+"?message="+message;
-    }
-    else{
-        window.location.href = page;
+    if (message) {
+        //window.location.href = page + "?message=" + message;
+    } else {
+        //window.location.href = page;
     }
 }
+
+async function connexion() {
+
+    const email = document.getElementById("emailCo").value;
+    const password = document.getElementById("motdepasseCo").value;
+
+    //const user = await new User(email, password)//.connect();
+    const user = await new User();
+    await user.connect()
+
+    console.log(await user.apikey)
+    console.log(await user.nom)
+
+    console.log(await user.me())
+
+    //const plann = await user.planning()
+    //const test = user.apikey
+    //const test2 = user.connect()
+    //const test3 = user.
+
+
+}
+
