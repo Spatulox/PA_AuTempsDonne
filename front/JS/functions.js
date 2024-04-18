8006f3740f66a4265a1f3b36e29bdb8dd26a6726
Spatulox <M78stormtrooper@laposte.net>
2024-04-18 11:23:22 +0200

    update function.js

--- front/JS/functions.js
+++ front/JS/functions.js
@@ -1,9 +1,9 @@
 // functions.js
 function redirect(page, message = null) {
     if (message) {
-        //window.location.href = page + "?message=" + message;
+        window.location.href = page + "?message=" + message;
     } else {
-        //window.location.href = page;
+        window.location.href = page;
     }
 }
 
@@ -12,20 +12,52 @@ async function connexion() {
     const email = document.getElementById("emailCo").value;
     const password = document.getElementById("motdepasseCo").value;
 
-    //const user = await new User(email, password)//.connect();
-    const user = await new User();
-    await user.connect()
+    //Première mannière de se connecter (avec email et mot de passe)
+    const premiereManiere = (async ()=>{
+        const user = await new User(email, password)//.connect();
+        await user.connect()
+    })
 
-    console.log(await user.apikey)
-    console.log(await user.nom)
+    // Second manière de se connecter : (sans email ni password)
+    const secondManiere = (async ()=> {
+        const user = await new User();
+        if(user.apikey == null){
+            return
+        }
+        await user.connect()
+        return user
+    })
 
-    console.log(await user.me())
 
-    //const plann = await user.planning()
-    //const test = user.apikey
-    //const test2 = user.connect()
-    //const test3 = user.
+    const user = await new User(email, password)
+    if(!await user.connect()){
+        popup("Impossible de se connecter :/ 🔁")
+        return
+    }
+
+    redirect("moncompte.php")
+    return
+
+    //alert("Update user")
+
+
+    // ----------- User -----------
+    //console.log(await user.me())
+    //console.log(await user.me(true))
+    //console.log(await user.getUser(id_user))
+    //console.log(await user.getAllUser())
+    //console.log(await user.getWaitingUser())
+    //await user.updateUser(lastname, firstname, phone, email)
+    //await user.deleteUser()
+    //await user.deleteUser(id_user)
+    //user.logout()
+
+    // --------- Planning ---------
+    //console.log(await user.planning())
+    //console.log(await user.allPlanning())
+
 
 
 }
+
 
