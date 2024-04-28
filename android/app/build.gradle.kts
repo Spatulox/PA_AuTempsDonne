0493de5b395419249c3bedf9b3a36b65ced5366c
Spatulox <M78stormtrooper@laposte.net>
2024-04-28 17:01:19 +0200

    add internet right to app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,4 +44,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Requests
+    implementation("com.android.volley:volley:1.2.1")
+
+    // Images
+    implementation("com.squareup.picasso:picasso:2.8")
 }
