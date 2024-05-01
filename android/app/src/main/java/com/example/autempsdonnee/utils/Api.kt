2205d81846a602052676441f9befc17a6919d52f
Spatulox <M78stormtrooper@laposte.net>
2024-05-01 18:57:03 +0200

    update api, add castToJson function

--- android/app/src/main/java/com/example/autempsdonnee/utils/Api.kt
+++ android/app/src/main/java/com/example/autempsdonnee/utils/Api.kt
@@ -1,6 +1,8 @@
 package com.example.autempsdonnee.utils
 
+import android.content.Context
 import com.android.volley.VolleyError
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
@@ -17,6 +19,44 @@ class Api {
             }
         } else {
             "Error when fetching API"
+        }
+    }
+
+    fun castToJson(context: Context, response: Any?): Any? {
+
+        if(response == null){
+            Popup().makeToast(context, "Data to cast are null")
+            return null
+        }
+
+        return try {
+            when (response) {
+                is JSONObject -> response
+                is JSONArray -> {
+                    if (response.length() == 1) {
+                        response.getJSONObject(0)
+                    } else {
+                        response
+                    }
+                }
+                else -> {
+                    val jsonData = JSONObject(response.toString())
+                    jsonData
+                }
+            }
+        } catch (e: JSONException) {
+            return try {
+                val jsonArray = JSONArray(response.toString())
+
+                if (jsonArray.length() == 1) {
+                    jsonArray.getJSONObject(0)
+                } else {
+                    jsonArray
+                }
+            } catch (e2: JSONException) {
+                Popup().makeToast(context, "Impossible to cast the data into JSON")
+                null
+            }
         }
     }
 }
