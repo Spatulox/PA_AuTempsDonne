ff5ddd003c05f70f8d8b0591b055e53103a771f7
Spatulox <M78stormtrooper@laposte.net>
2024-05-02 17:17:32 +0200

    update main.go

--- tickets/main.go
+++ tickets/main.go
@@ -1,11 +1,30 @@
-package tickets
+package main
 
 import (
+	. "tickets/pkg/const"
+	. "tickets/pkg/manager"
 	. "tickets/pkg/web"
 )
 
 func main() {
 
 	// Start a goroutine for an asynchronous listen to have the CLI
+	//go EnableHandlers()
 	go EnableHandlers()
+
+	Log.Debug("Coucou")
+
+	RecupTickets(nil)
+
+	err := UpdateTicketsEtape(4, 2)
+
+	if !err {
+		Log.Error("C'est false")
+	}
+
+	err = UpdateTicketsCategorie(5, 6)
+
+	if !err {
+		Log.Error("C'est false")
+	}
 }
