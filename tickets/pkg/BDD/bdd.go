736cef037df299a89e909b9327044b35a86aa4c1
Spatulox <M78stormtrooper@laposte.net>
2024-05-02 17:16:30 +0200

    update bdd.go

--- tickets/pkg/BDD/bdd.go
+++ tickets/pkg/BDD/bdd.go
@@ -17,7 +17,7 @@ type Db struct {
 // Renvoie une structure database
 func connectDB() (db *sql.DB, errG error) {
 
-	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/go_reserv")
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/apiDev_db")
 	if err != nil {
 		Log.Error("Impossible de se connecter à la BDD", err)
 		return nil, errG
