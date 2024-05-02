dcfece1f2d08175d3f02cdbc6201e5ef7b7b6297
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-05-02 10:00:44 +0200

    Update model Stock

--- api/Models/stockModel.php
+++ api/Models/stockModel.php
@@ -6,32 +6,33 @@ class StockModel
 {
     public $id_stock;
     public $quantite_produit;
-    public $date_entre;
+    public $date_entree;
     public $date_sortie;
     public $date_peremption;
     public $desc_produit;
     public $id_produit;
-    public $id_entrepots;
-    public $entrepot_desc;
     public $produit_desc;
+    public $id_entrepot;
+    public $entrepot_desc;
 
-    public function __construct($id_stock, $quantite_produit, $date_entre, $date_sortie, $date_peremption,$desc_produit,$id_produit,$id_entrepots) {
+
+    public function __construct($id_stock, $quantite_produit, $date_entree, $date_sortie, $date_peremption,$desc_produit,$id_produit,$id_entrepot) {
         $this->id_stock = $id_stock;
         $this->quantite_produit = $quantite_produit;
-        $this->date_entre = $date_entre;
+        $this->date_entree = $date_entree;
         $this->date_sortie = $date_sortie;
         $this->date_peremption = $date_peremption;
         $this->desc_produit = $desc_produit;
         $this->id_produit = $id_produit;
-        $this->id_entrepots = $id_entrepots;
+        $this->id_entrepot = $id_entrepot;
     }
 
-    public function setentrepot(){
-        $this->entrepot_desc = [0];
+    public function setentrepot($ENTREPOTS){
+        $this->entrepot_desc = $ENTREPOTS[0];
     }
 
-    public function setIndexProduit(){
-        $this->produit_desc = [0];
+    public function setIndexProduit($produit){
+        $this->produit_desc = $produit[0];
     }
 
 }
