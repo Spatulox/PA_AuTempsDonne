22816a8ee16873162569fbd07610c8023a7df69d
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-05-02 15:56:10 +0200

    version pas finie du post createStock

--- api/Service/stockService.php
+++ api/Service/stockService.php
@@ -5,6 +5,21 @@ include_once './index.php';
 
 class StockService {
 
+    function isValidDate($date) {
+        $pattern = '/^(\d{4})-(\d{2})-(\d{2})$/';
+        if (preg_match($pattern, $date, $matches)) {
+            $year = $matches[1];
+            $month = $matches[2];
+            $day = $matches[3];
+            if (checkdate($month, $day, $year)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//----------------------------------------------------------------------------------
+
     public function getAllStock($apiKey)
     {
         $userRole = getRoleFromApiKey($apiKey);
@@ -17,12 +32,94 @@ class StockService {
 
     }
 
+//----------------------------------------------------------------------------------
+
     public function getAllStockInEntrepots($id, $apiKey)
     {
         $userRole = getRoleFromApiKey($apiKey);
         if ($userRole[0]==1 || $userRole[0]==2 || $userRole[0]==3) {
         $stockRepository = new StockRepository();
         return $stockRepository->getAllStockInEntrepots($id);
+        }else{
+            exit_with_message("Vous n'avais pas accès a cette commande");
+        }
+    }
+
+//--------------------------------------------------------------------------------------------
+
+    public function getAllProduitsInEntrepots($id,$produit, $apiKey)
+    {
+        $userRole = getRoleFromApiKey($apiKey);
+        if ($userRole[0]==1 || $userRole[0]==2 || $userRole[0]==3) {
+            $stockRepository = new StockRepository();
+            return $stockRepository->getAllProduitsInEntrepots($id,$produit);
+        }else{
+            exit_with_message("Vous n'avais pas accès a cette commande");
+        }
+    }
+
+//----------------------------------------------------------------------------------
+
+    public function createStock(StockModel $stock, $apiKey)
+    {
+        $stockRepository = new StockRepository();
+
+        $userRole = getRoleFromApiKey($apiKey);
+        if ($userRole[0]==1 || $userRole[0]==2 || $userRole[0]==3) {
+
+            $string = "id_entrepot=" .$stock->id_entrepot;
+            $entrepots= selectDB("ENTREPOTS", "id_entrepot",$string,"bool");
+
+            if(!$entrepots){
+                exit_with_message("cette entrepot n'existe pas ", 500);
+            }
+
+            $string = "id_produit=" .$stock->id_produit ;
+            $produit= selectDB("PRODUIT", "id_produit",$string,"bool");
+
+            if(!$produit){
+                exit_with_message("ce produit n'existe pas ", 500);
+            }
+
+            if (($stock->date_sortie == "NULL" && $stock->date_entree =="NULL")) {
+                exit_with_message("Erreur : il faut rentre une date", 400);
+            }
+
+            if (($stock->date_sortie != "NULL" && $stock->date_entree !="NULL")) {
+                exit_with_message("Erreur : il ne faut pas deux date", 400);
+            }
+
+
+            if (($stock->date_sortie == "NULL" && $stock->date_entree !="NULL")) {
+                if (!$this->isValidDate($stock->date_entree)) {
+                exit_with_message("Erreur : La date d'entrée est invalide. Le format attendu est AAAA-MM-JJ.", 403);
+                }
+            }
+
+            if (($stock->date_sortie != "NULL" && $stock->date_entree =="NULL")) {
+
+                if (!$this->isValidDate($stock->date_sortie)) {
+                exit_with_message("Erreur : La date de sortie est invalide. Le format attendu est AAAA-MM-JJ.", 403);
+                }
+
+                $test = $this->getAllProduitsInEntrepots($stock->id_entrepot,$stock->id_produit,$apiKey);
+
+                $id_produit_stock = array();
+                foreach ($test as $item) {
+                    $id_produit_stock[] = $item->id_stock;
+                }
+                return $id_produit_stock;
+            }
+
+            if ($stock->date_peremption == "NUlL"){
+                return $stockRepository->createStock($stock);
+             } else if (!$this->isValidDate($stock->date_peremption)) {
+                exit_with_message("Erreur : La date de péremption est invalide. Le format attendu est AAAA-MM-JJ.", 403);
+            }
+
+
+
+            return $stockRepository->createStock($stock);
         }else{
             exit_with_message("Vous n'avais pas accès a cette commande");
         }
