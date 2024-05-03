fbe15f330ab60aa00e0a2101352d240a28c08b71
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-05-03 20:04:54 +0200

    Upload vehiculeModel

--- api/Models/vehiculeModel.php
+++ api/Models/vehiculeModel.php
@@ -2,6 +2,18 @@
 
 class VehiculeModel
 {
-
+    public $id_vehicule;
+    public $capacite;
+    public $id_nom_du_vehicule;
+    public $nombre_de_place;
+    public $id_entrepot;
+    
+    public function __construct($id_vehicule, $capacite, $nombre_de_place, $id_nom_du_vehicule)
+    {
+        $this->id_vehicule = $id_vehicule;
+        $this->capacite = $capacite;
+        $this->nombre_de_place = $nombre_de_place;
+        $this->id_nom_du_vehicule = $id_nom_du_vehicule;
+    }
 }
 ?>
