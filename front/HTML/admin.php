7626904ec7687356410c32f6959712b5a09c39cf
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-05-04 12:53:31 +0200

    add  idée page

---
+++ front/HTML/admin.php
@@ -0,0 +1,32 @@
+<?php include("../includes/loadLang.php");?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<?php include("../includes/head.php"); ?>
+
+		<title><?php echo($data["template"]["title"]) ?></title>
+	</head>
+	<body>
+
+		<?php include("../includes/header.php");?>
+
+		<main>
+
+			<section class="flex flexCenter wrap">
+				<h1 class="width100 textCenter noMarginBottom"><?php echo($data["template"]["sectionTitle"]) ?></h1>
+			</section>
+
+			<?php
+
+			?>
+
+		</main>
+
+		<?php include("../includes/footer.php");?>
+
+	</body>
+</html>
+
+
