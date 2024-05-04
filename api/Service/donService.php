d87d9e4074b05b83107e44e9d67ec3eabea6efc8
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-05-04 15:54:38 +0200

    Update donService add full Get

--- api/Service/donService.php
+++ api/Service/donService.php
@@ -1,7 +1,53 @@
 <?php
 
+include_once './Repository/donRepository.php';
+include_once './index.php';
+
 class DonService
 {
 
+    public function getDonById($id, $apiKey)
+    {
+        $userRole = getRoleFromApiKey($apiKey);
+        if ($userRole==1 || $userRole==2 ) {
+            $donRepository = new DonRepository();
+            return $donRepository->getDonById($id);
+        }else{
+            exit_with_message("You didn't have access to this command");
+        }
+    }
+    //-------------------------------------------------------------------------------------------
+    public function getDonEntrepot($apiKey)
+    {
+        $userRole = getRoleFromApiKey($apiKey);
+        if ($userRole==1 || $userRole==2 ) {
+            $donRepository = new DonRepository();
+            return $donRepository->getDonEntrepot();
+        }else{
+            exit_with_message("You didn't have access to this command");
+        }
+    }
+    //-------------------------------------------------------------------------------------------
+    public function getDonAnnuel($date, $apiKey)
+    {
+        $userRole = getRoleFromApiKey($apiKey);
+        if ($userRole==1 || $userRole==2 ) {
+            $donRepository = new DonRepository();
+            return $donRepository->getDonAnnuel($date);
+        }else{
+            exit_with_message("You didn't have access to this command");
+        }
+    }
+    //-------------------------------------------------------------------------------------------
+    public function getDonMensuel($date, $apiKey)
+    {
+        $userRole = getRoleFromApiKey($apiKey);
+        if ($userRole==1 || $userRole==2 ) {
+            $donRepository = new DonRepository();
+            return $donRepository->getDonMensuel($date);
+        }else{
+            exit_with_message("You didn't have access to this command");
+        }
+    }
 }
 ?>
