db2d741ada14701107b543f1abf20616bd9bf514
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-05-04 15:33:05 +0200

    update don models

--- api/Models/donModels.php
+++ api/Models/donModels.php
@@ -2,6 +2,18 @@
 
 class DonModels
 {
+    public $id_don;
+    public $prix;
+    public $date_don;
+    public $id_user;
 
+    public function __construct($id_don, $prix, $date_don, $id_user)
+    {
+        $this->id_don = $id_don;
+        $this->prix = $prix;
+        $this->date_don = $date_don;
+        $this->id_user = $id_user;
+    }
+    
 }
 ?>
