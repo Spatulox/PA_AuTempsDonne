3fa533791311b042314c9d51208969342a380178
Spatulox <M78stormtrooper@laposte.net>
2024-05-08 13:04:37 +0200

    update admin.php

--- front/HTML/admin.php
+++ front/HTML/admin.php
@@ -8,7 +8,7 @@
 
 		<?php include("../includes/head.php"); ?>
 
-		<title><?php echo($data["template"]["title"]) ?></title>
+		<title><?php echo($data["admin"]["title"]) ?></title>
 	</head>
 	<body>
 
@@ -17,17 +17,37 @@
 		<main>
 
 			<section class="flex flexCenter wrap">
-				<h1 class="width100 textCenter noMarginBottom"><?php echo($data["template"]["sectionTitle"]) ?></h1>
+				<h1 class="width100 textCenter noMarginBottom"><?php echo($data["admin"]["title"]) ?></h1>
 			</section>
 
-            <div id="ticket">
-                <h1><?php echo($data["ticket"]["title"]) ?></h1>
-                <ul>
-                    <li><a style="color: #0c2124" href="<?php echo($address["ticket"]) ?>/list?me=true" target="_blank"><?php echo($data["ticket"]["me"]) ?></a></li>
-                    <li><a style="color: #0c2124" href="<?php echo($address["ticket"]) ?>/list?assign=false" target="_blank"><?php echo($data["ticket"]["assignFalse"]) ?></a></li>
-                    <li><a style="color: #0c2124" href="<?php echo($address["ticket"]) ?>/list" target="_blank"><?php echo($data["ticket"]["all"]) ?></a></li>
-                </ul>
-            </div>
+            <section class="flex flexAround alignCenter height90">
+                <div id="ticket" class="box width20">
+                    <h1 class="textCenter bold underline"><?php echo($data["ticket"]["title"]) ?></h1>
+                    <ul class="ul">
+                        <li><a style="color: #0c2124" href="<?php echo($address["ticket"]) ?>/list?me=true" target="_blank"><?php echo($data["ticket"]["me"]) ?></a></li>
+                        <li><a style="color: #0c2124" href="<?php echo($address["ticket"]) ?>/list?assign=false" target="_blank"><?php echo($data["ticket"]["assignFalse"]) ?></a></li>
+                        <li><a style="color: #0c2124" href="<?php echo($address["ticket"]) ?>/list" target="_blank"><?php echo($data["ticket"]["all"]) ?></a></li>
+                    </ul>
+                </div>
+
+                <div id="users" class="box width20">
+                    <h1 class="textCenter bold underline"><?php echo($data["user"]["title"]) ?></h1>
+                    <ul class="ul">
+                        <li><a style="color: #0c2124" href="" target="_blank"><?php echo($data["user"]["title"]) ?></a></li>
+                        <li><a style="color: #0c2124" href="" target="_blank"><?php echo($data["user"]["title"]) ?></a></li>
+                        <li><a style="color: #0c2124" href="" target="_blank"><?php echo($data["user"]["title"]) ?></a></li>
+                    </ul>
+                </div>
+
+                <div id="storehouse" class="box width20">
+                    <h1 class="textCenter bold underline"><?php echo($data["storehouse"]["title"]) ?></h1>
+                    <ul class="ul">
+                        <li><a style="color: #0c2124" href="" target="_blank"><?php echo($data["storehouse"]["title"]) ?></a></li>
+                        <li><a style="color: #0c2124" href="" target="_blank"><?php echo($data["storehouse"]["title"]) ?></a></li>
+                        <li><a style="color: #0c2124" href="" target="_blank"><?php echo($data["storehouse"]["title"]) ?></a></li>
+                    </ul>
+                </div>
+            </section>
 
 		</main>
 
