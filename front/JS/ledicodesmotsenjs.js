1ed312431e74d9c88b38b3ff13c8bc982d795686
Spatulox <M78stormtrooper@laposte.net>
2024-05-08 19:58:54 +0200

    update some langs

--- front/JS/ledicodesmotsenjs.js
+++ front/JS/ledicodesmotsenjs.js
@@ -1,8 +1,21 @@
 const dico = {
     "EN":{
+        "msg":{
+            "update":"Update",
+            "success":"succed",
+            "failed":"failed"
+        },
+        "buttonSee":"See",
+        "updateUser":"Update User"
 
     },
     "FR":{
-
+        "msg":{
+            "update":"Mise à jour",
+            "success":"terminé(e)",
+            "failed":"échoué"
+        },
+        "buttonSee":"Voir",
+        "updateUser":"Mettre à jour l'utilistaeur"
     }
 }
