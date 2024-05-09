092196f5d8dac43d31efda6e28d52ab4b3d695ba
Spatulox <M78stormtrooper@laposte.net>
2024-05-10 01:01:02 +0200

    update gestPlanning.php

--- front/HTML/gestPlanning.php
+++ front/HTML/gestPlanning.php
@@ -20,19 +20,15 @@
             <h2 class="underline"><?php echo($data["gestPlanning"]["tab1"]["title"]) ?></h2>
             <p id="popupGestionTitle">Des trucs</p>
             <hr>
-            <h4><?php echo($data["gestPlanning"]["tab1"]["participateUsers"]) ?> :</h4>
+            <h4 id="h4"><?php echo($data["gestPlanning"]["tab1"]["participateUsers"]) ?> : 0</h4>
             <p id="popupGestionBody">
 
             </p>
-
-            <div class="flex flexAround">
+            <p id="select"></p>
+            <div class="flex flexAround nowrap">
                 <button type="button" onclick="addUserPopup()"><?php echo($data["gestPlanning"]["addUser"]) ?></button>
-                <button type="button"><?php echo($data["gestPlanning"]["assign"]) ?></button>
+                <button type="button" onclick="assignUser()"><?php echo($data["gestPlanning"]["assign"]) ?></button>
             </div>
-        </div>
-        <div class="popupGestion-content">
-            <span class="close-button">&times;</span>
-            <ul>Coucou</ul>
         </div>
     </div>
 
@@ -67,6 +63,7 @@
 
         <div id="tab2" class="tabcontent">
             <h3><?php echo($data["gestPlanning"]["tab2"]["title"]) ?></h3>
+            <input id="inputDate" type="date" class="marginBottom20 search-box" oninput="searchDaily()">
             <table>
                 <thead>
                 <tr>
@@ -130,38 +127,61 @@
 
 <script>
     const closeButton = document.getElementsByClassName("close-button")[0];
-    const closeButton2 = document.getElementsByClassName("close-button")[1];
 
     closeButton.addEventListener("click", () => {
         const popup = document.getElementById("popupGestion");
         popup.style.display = "none";
     });
 
-    closeButton2.addEventListener("click", () => {
-        const popup = document.getElementById("popupGestion");
-        popup.style.display = "none";
-    });
 </script>
 
 <script type="text/javascript" defer>
 
     const planning = new PlanningAdmin()
+    const user = new UserAdmin()
+
+    let idPlanningToSend = 0
+
+    let users = []
+
+    let emailToAssign = []
 
     let validatePlanningData = {}
 
     let dailyPlanning = {}
 
     async function addUserPopup() {
+
         const fieldMain = document.getElementById("popupGestionBody");
 
         // Créer le conteneur de l'élément
         const elementContainer = document.createElement("div");
         elementContainer.classList.add("element-container");
 
+        const idSelectValue = document.getElementById("idSelect").value
+
+        if(idSelectValue === "Choisisser une addresse email"){
+            return
+        }
+
+
+        if(emailToAssign.includes(idSelectValue)){
+            popup("Vous ne pouvez pas attribuer une même activité au même utilisateur plusieurs fois :/")
+            return
+        }
+        emailToAssign.push(idSelectValue)
+
+        let header = document.getElementById("h4").innerHTML
+        header = header.split(" : ")
+        header[1] = +(header[1].trim())
+        header[1] += 1
+        header = header.join(" : ")
+        document.getElementById("h4").innerHTML = header
+
         // Créer l'élément
         const element = document.createElement("span");
         element.classList.add("element");
-        element.textContent = "Entrez une valeur";
+        element.textContent = idSelectValue;
 
         // Créer la coche
         const checkmark = document.createElement("span");
@@ -171,7 +191,19 @@
 
         // Ajouter un écouteur d'événement pour supprimer l'élément
         checkmark.addEventListener("click", () => {
+            const index = emailToAssign.indexOf(idSelectValue);
+            if (index !== -1) {
+                emailToAssign.splice(index, 1);
+            }
             elementContainer.remove();
+
+            let header = document.getElementById("h4").innerHTML
+            header = header.split(" : ")
+            header[1] = +(header[1].trim())
+            header[1] -= 1
+            header = header.join(" : ")
+            document.getElementById("h4").innerHTML = header
+
         });
 
         // Ajouter l'élément et la coche au conteneur
@@ -182,13 +214,68 @@
         fieldMain.appendChild(elementContainer);
     }
 
+    async function assignUser(){
+
+        if(emailToAssign.length === 0){
+            popup("Vous devez assigner au moins un utilisateur")
+            return
+        }
+        //Requête API
+
+        if(idPlanningToSend === 0){
+            popup("Erreur lors de la sélection du planning, veuillez rafraichir la page")
+            return
+        }
+
+        startLoading()
+        for (const em in emailToAssign) {
+            leEmail = emailToAssign[em]
+
+            idUser = await user.getUserViaEmail(leEmail)
+
+            await planning.userJoinPlanning(idUser.id_user, idPlanningToSend)
+
+        }
+        stopLoading()
+
+        idPlanningToSend = 0
+
+        let header = document.getElementById("h4").innerHTML
+        header = header.split(" : ")
+        header[1] = +(header[1].trim())
+        header[1] = 0
+        header = header.join(" : ")
+        document.getElementById("h4").innerHTML = header
+
+        emailToAssign = []
+        const popupG = document.getElementById("popupGestion");
+        popupG.style.display = "none";
+
+        startLoading()
+        await fillAssignPlanning()
+        await fillDailyPlanning()
+        stopLoading()
+
+    }
 
 
     async function assignPlanning(id){
 
-        const popup = document.getElementById("popupGestion");
+        emailToAssign = []
+
         startLoading()
 
+        idPlanningToSend = id
+
+        let header = document.getElementById("h4").innerHTML
+        header = header.split(" : ")
+        header[1] = +(header[1].trim())
+        header[1] = 0
+        header = header.join(" : ")
+        document.getElementById("h4").innerHTML = header
+
+
+        const popupG = document.getElementById("popupGestion");
         let data = validatePlanningData
         data.forEach(item => {
             if(item.id_planning == id){
@@ -197,55 +284,76 @@
             }
         })
 
-        console.log(data)
-
         const popupTitle = document.getElementById("popupGestionTitle")
         popupTitle.innerHTML = data.description + " - " + data.date_activite
-        document.getElementById("popupGestionBody").innerHTML = ""
-        //addUserPopup()
+        popupBody = document.getElementById("popupGestionBody")
+        popupBody.innerHTML = ""
+
+        await updateListUser(data.date_activite)
 
 
         replaceCharacters()
         stopLoading()
-        popup.style.display = "flex";
+        popupG.style.display = "flex";
     }
 
     async function fillWaitingPlanning(){
         const tbody = document.getElementById("bodyWaiting")
         tbody.innerHTML = ""
         const waitingPlanning = await planning.getWaitPlanning()
-        createBodyTableau(tbody,waitingPlanning, ["id_index_planning", "id_activite"], [planning.msg["Validate"], planning.msg["Delete"]], ["validatePlanning", "deletePlanning"], "id_planning")
-        replaceCharacters()
+
+        if(waitingPlanning.length > 0) {
+            createBodyTableau(tbody, waitingPlanning, ["id_index_planning", "id_activite"], [planning.msg["Validate"], planning.msg["Delete"]], ["validatePlanning", "deletePlanning"], "id_planning")
+            replaceCharacters()
+        }
     }
 
     async function fillAssignPlanning(){
         const tbody = document.getElementById("bodyAssign")
         tbody.innerHTML = ""
         validatePlanningData = await planning.getNoAffectPlanning()
-        createBodyTableau(tbody, validatePlanningData, ["id_index_planning", "id_activite"], [planning.msg["Assign"]], ["assignPlanning"], "id_planning")
-        replaceCharacters()
+
+        if(validatePlanningData.length > 0) {
+            createBodyTableau(tbody, validatePlanningData, ["id_index_planning", "id_activite"], [planning.msg["Assign"]], ["assignPlanning"], "id_planning")
+            replaceCharacters()
+        }
     }
 
-    async function fillDailyPlanning(){
+    async function searchDaily(){
+        let dateStr = document.getElementById("inputDate").value
+        dateStr = dateStr.split("T")
+        fillDailyPlanning(dateStr)
+    }
 
-        const today = new Date();
-        const year = today.getFullYear();
-        let month = today.getMonth() + 1; // Les mois sont indexés à partir de 0
-        let day = today.getDate();
+    async function fillDailyPlanning(dateStr = null){
 
-        if(month < 10){
-            month = "0"+month
+        let formattedDate = ""
+        if(dateStr == null){
+            const today = new Date();
+            const year = today.getFullYear();
+            let month = today.getMonth() + 1; // Les mois sont indexés à partir de 0
+            let day = today.getDate();
+
+            if(month < 10){
+                month = "0"+month
+            }
+            if(day < 10){
+                day = "0"+day
+            }
+            formattedDate = `${year}-${month}-${day}`;
+        } else {
+            formattedDate = dateStr
         }
-        if(day < 10){
-            day = "0"+day
-        }
-        const formattedDate = `${year}-${month}-${day}`;
 
         const tbody = document.getElementById("bodyDaily")
         tbody.innerHTML = ""
         const dailyPlanning = await planning.getAffectByDatePlanning(formattedDate)
+        console.log(formattedDate)
+        console.log(dailyPlanning)
 
-        createBodyTableau(tbody, dailyPlanning, ["id_index_planning", "id_activite"])
+        if(dailyPlanning.length > 0){
+            createBodyTableau(tbody, dailyPlanning, ["id_index_planning", "id_activite"])
+        }
         replaceCharacters()
     }
 
@@ -256,9 +364,38 @@
         stopLoading()
     }
 
+    async function updateListUser(dateStr){
+        const date = new Date(dateStr);
+        const jourSemaine = date.getDay();
+        users = await user.getUserDispoByDay(jourSemaine+1)
+
+        const selectBody = document.getElementById("select")
+        selectBody.innerHTML = ""
+
+        let options = []
+
+        let i = 0
+        for (const key in users) {
+            options[i] ={
+                "value": users[key].email,
+                "text": users[key].email
+            }
+            i++
+        }
+
+        const select = createSelect(options, "Choisisser une addresse email")
+        select.id = "idSelect"
+        selectBody.appendChild(select)
+
+    }
+
     async function onload() {
         startLoading()
         await planning.connect()
+        await user.connect()
+
+        document.getElementById("inputDate").value = ""
+
 
         fillAssignPlanning()
         fillDailyPlanning()
