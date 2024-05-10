e1fa6177e5242f623795ea24a27825560aa132f2
Spatulox <M78stormtrooper@laposte.net>
2024-05-10 16:03:30 +0200

    update storehouse.php

--- front/HTML/storehouse.php
+++ front/HTML/storehouse.php
@@ -24,7 +24,7 @@
         </div>
 
         <div id="tab1" class="tabcontent marginBottom20">
-            <h3 class="flex flexCenter"><?php echo($data["storehouse"]["tab1"]["title"]) ?></h3>
+            <h2 class="flex flexCenter"><?php echo($data["storehouse"]["tab1"]["title"]) ?></h2>
             <table class="">
                 <thead>
                 <tr>
@@ -41,12 +41,32 @@
         </div>
 
         <div id="tab2" class="tabcontent marginBottom20">
-            <h3 class="flex flexCenter"><?php echo($data["storehouse"]["tab2"]["title"]) ?></h3>
+            <h2 class="flex flexCenter"><?php echo($data["storehouse"]["tab2"]["title"]) ?></h2>
             <p class="border marginBottom10" id="bodyDetail"><?php echo($data["storehouse"]["tab2"]["errorMsg"]) ?> <button type="button" class="width100" onclick="openTab('tab1')"><?php echo($data["storehouse"]["tab1"]["title"]) ?></button> </p>
         </div>
 
         <div id="tab3" class="tabcontent marginBottom20">
-            <h3 class="flex flexCenter"><?php echo($data["storehouse"]["tab3"]["title"]) ?></h3>
+            <h2 class="textCenter"><?php echo($data["storehouse"]["tab3"]["title"]) ?></h2>
+            <hr class="marginBottom40">
+            <h3 class="textCenter"><?php echo($data["storehouse"]["tab3"]["generalInfo"]) ?></h3>
+            <ul class="border marginBottom10 noDecoration" id="bodyCreateGeneralInfo">
+                <li><input class="marginTop10" type="text" placeholder="<?php echo($data["storehouse"]["tab1"]["name"]) ?>"></li>
+                <li><input class="marginTop10" type="number" placeholder="<?php echo($data["storehouse"]["tab1"]["ParkingPlace"]) ?>"></li>
+            </ul>
+            <h3 class="textCenter"><?php echo($data["storehouse"]["tab1"]["Addresses"]) ?></h3>
+            <ul class="border marginBottom10 noDecoration" id="bodyCreateAddress">
+                <li><input class="marginTop10" type="text" placeholder="<?php echo($data["storehouse"]["tab3"]["city"]) ?>"></li>
+                <li><input class="marginTop10" type="text" placeholder="<?php echo($data["storehouse"]["tab1"]["Addresses"]) ?>"></li>
+                <li><input class="marginTop10" type="number" placeholder="<?php echo($data["storehouse"]["tab1"]["postal"]) ?>"></li>
+                <li><input class="marginTop10" type="text" placeholder="<?php echo($data["storehouse"]["tab3"]["complementary"]) ?>"></li>
+            </ul>
+
+            <h3 class="textCenter"><?php echo($data["storehouse"]["tab2"]["createShelf"]) ?></h3>
+            <ul class="border marginBottom10 noDecoration">
+                <li><input id="inputPlaceCreate" class="marginTop10 marginBottom10" type="number" placeholder="<?php echo($data["storehouse"]["tab1"]["shelf"]) ?>"></li>
+            </ul>
+
+            <input class="marginTop30 marginBottom30 marginAuto block" type="button" value="<?php echo($data["storehouse"]["tab3"]["title"]) ?>" onclick="createEntrepot()">
 
         </div>
     </div>
@@ -80,6 +100,7 @@
 
     async function fillListing(){
         const bodyList = document.getElementById("bodyList")
+        bodyList.innerHTML =""
         dataEntrepot = await entrepot.getEntrepot()
         createBodyTableau(bodyList, dataEntrepot,["id_addresse"], [entrepot.msg["See"]], ["seeEntrepot"], "id_entrepot")
         replaceCharacters()
@@ -98,10 +119,19 @@
 
 
         const formattedEntrepot = formatEntrepot(data[0]);
-
         const bodyDetail = document.getElementById("bodyDetail")
-
         bodyDetail.innerHTML = formattedEntrepot
+
+        let remaining = document.getElementById("placeDispo").innerHTML
+        remaining = remaining.split(" :")
+        console.log(remaining)
+
+        const place = await entrepot.getPlaceDispoEntrepot(id)
+        console.log()
+
+        remaining[1] = place.message
+        remaining = remaining.join(" : ")
+        document.getElementById("placeDispo").innerHTML = remaining
 
         replaceCharacters()
 
@@ -149,9 +179,69 @@
         seeEntrepot(+premierPElement)
     }
 
+    async function createEntrepot(){
+
+        startLoading()
+        const inputsTab3 = document.querySelectorAll('#tab3 input');
+
+        for (const key in inputsTab3) {
+            if(inputsTab3[key].value === "" && inputsTab3[key] !== inputsTab3[5]){
+                popup('"'+inputsTab3[key].placeholder + '" of the storehouse need to be filled')
+                stopLoading()
+                return
+            }
+
+        }
+
+        const address = inputsTab3[3].value + ", " + inputsTab3[4].value + " " + inputsTab3[2].value
+
+        const add = await entrepot.createAddress(address)
+
+        if(add === false){
+            stopLoading()
+            return
+        }
+
+        const data = {
+            "nom_entrepot":inputsTab3[0].value,
+            "parking":inputsTab3[1].value,
+            "id_adresse":add.id_adresse,
+            "etagere":[{
+                "nombre_de_place":inputsTab3[6].value
+            }]
+        }
+
+        const entr = await entrepot.createEntrepot(data)
+
+        if(entr === false){
+            await entrepot.deleteAddress(add.id_adresse)
+        }
+
+        fillListing()
+
+        stopLoading()
+
+    }
+
+    async function deleteEntrepot(id){
+        startLoading()
+
+        const entre = await entrepot.getEntrepot(id)
+
+        const rep = await entrepot.deleteEntrepot(id)
+
+
+        const resp = await entrepot.deleteAddress(entre[0].id_addresse)
+
+        fillListing()
+
+        stopLoading()
+    }
+
     function formatEntrepot(entrepotV) {
         let formattedEntrepot = `<div class="entrepot">
             <h2>${entrepotV.nom}</h2>
+            <h3 id="placeDispo" class="underline bold"><?php echo($data["storehouse"]["tab2"]["availablePlace"]) ?> :</h3>
             <p><?php echo($data["storehouse"]["tab1"]["IDStorehouse"]) ?> : ${entrepotV.id_entrepot}</p>
             <p><?php echo($data["storehouse"]["tab1"]["Addresses"]) ?> : ${entrepotV.addresse_desc}</p>
             <p><?php echo($data["storehouse"]["tab1"]["ParkingPlace"]) ?> : ${entrepotV.parking}</p>
@@ -178,7 +268,13 @@
               `<input type="button" onclick="createShelf(${entrepotV.id_entrepot})" value="${entrepot.msg["Create"]}"><br>`+
               "</div>"
 
+
+
+        let deleteEntre = `<hr><h1 class="textCenter">DELETE STOREHOUSE</h1><div><input class="marginAuto block" type="button" onclick="deleteEntrepot(${entrepotV.id_entrepot})" value="${entrepot.msg["Delete"]}"></div>`
+
         formattedEntrepot += createShelf
+
+        formattedEntrepot += deleteEntre
 
         return formattedEntrepot;
     }
