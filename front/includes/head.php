505b03e9ea1987ac839d62804b1050aa7cdc85ca
Spatulox <M78stormtrooper@laposte.net>
2024-05-12 01:26:51 +0200

    add gestionActivite js

--- front/includes/head.php
+++ front/includes/head.php
@@ -31,6 +31,7 @@ if($role == 1 || $role == 2){
     echo '<script type="text/javascript" src="../JSManager/GestionDon.js"></script>';
     echo '<script type="text/javascript" src="../JSManager/GestionTrajet.js"></script>';
     echo '<script type="text/javascript" src="../JSManager/GestionDemandes.js"></script>';
+    echo '<script type="text/javascript" src="../JSManager/GestionActivite.js"></script>';
 
     echo '<script type="text/javascript" src="../JS/qrcode.js"></script>';
     echo '<script type="text/javascript" src="../JS/pdfmake.js"></script>';
