f9f4e85a7cb956e72491d2338baae9d69a390492
Spatulox <M78stormtrooper@laposte.net>
2024-05-11 19:47:02 +0200

    update lanning.php

--- front/HTML/planning.php
+++ front/HTML/planning.php
@@ -18,9 +18,18 @@
 				<h1 class="width100 textCenter noMarginBottom"><?php echo($data["planning"]["sectionTitle"]) ?></h1>
 			</section>
 
-			<?php
-
-			?>
+            <table class="width80 marginAuto marginTop40 border">
+                <thead>
+                <tr>
+                    <td>Description activité</td>
+                    <td>Date</td>
+                    <td>Type / Desc</td>
+                    <td>Localisation</td>
+                </tr>
+                </thead>
+                <tbody id="tbodyUser">
+                </tbody>
+            </table>
 
 		</main>
 
@@ -28,3 +37,26 @@
 
 	</body>
 </html>
+
+<script type="text/javascript" defer>
+    const user = new User()
+    let planningData = []
+
+    async function fillArray(){
+        const tbodyUser = document.getElementById("tbodyUser")
+        tbodyUser.innerHTML = ""
+        planningData = await user.myplanning()
+        console.log(planningData)
+
+        createBodyTableau(tbodyUser, planningData, ["id_activite", "id_index_planning", "id_planning", "nom_index_planning", "user"])
+
+    }
+
+    async function onload(){
+        startLoading()
+        fillArray()
+        stopLoading()
+    }
+
+    onload()
+</script>
