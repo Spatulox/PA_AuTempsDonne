2632ae36fcd1dbbc13cfe9dae19d2fafeb466d07
Spatulox <M78stormtrooper@laposte.net>
2024-08-13 18:35:27 +0200

    cleaning StripePayement.php

--- api/stripe/StripePayement.php
+++ api/stripe/StripePayement.php
@@ -4,8 +4,6 @@ use \Stripe\Stripe;
 use Stripe\Checkout\Session;
 class StripePayement
 {
-
-
     private string $clientToken;
 
     public function __construct(string $clientToken){
@@ -14,6 +12,8 @@ class StripePayement
         Stripe::setApiVersion('2024-06-20');
     }
 
+    // Create th Checkout on stripe to the user to pay
+    // Send the result to a JS for the js to "redirect"
     public function startPayement($amounts, $names, $mode = "payment")
     {
         foreach ($amounts as $index => $amount) {
@@ -31,16 +31,7 @@ class StripePayement
 
         try {
             $session = Session::create([
-                    'line_items' => [$line_items/*[
-                        'quantity' => 1,
-                        'price_data' => [
-                            'currency' => 'EUR',
-                            'product_data' => [
-                                'name' => $name
-                            ],
-                            'unit_amount' => $amount * 100, // Faut le mettre en centimes
-                        ]
-                    ]*/],
+                    'line_items' => [$line_items],
                     'mode' => $mode,
                     'success_url' => "http://localhost:8083/HTML/moncompte.php?message=Votre payement a ete realise avec succes",
                     'cancel_url' => "http://localhost:8083/HTML/moncompte.php?message=Votre payement n'a pas abouti",
@@ -52,13 +43,7 @@ class StripePayement
 
             );
 
-        /*    header("HTTP/1.1 303 See Other");
-            header('Location: ' . $session->url);
-        } catch (Exception $e) {
-            exit_with_message($e->getMessage());
-        }*/
-
-        // Retourner l'URL de la session au lieu de rediriger
+            // Retourner l'URL de la session et l'id
             exit_with_content([
                 'sessionId' => $session->id,
                 'url' => $session->url
