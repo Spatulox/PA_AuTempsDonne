99d1017a39ca18d62424ce4be9d752c267153f10
Spatulox <M78stormtrooper@laposte.net>
2024-08-13 18:26:41 +0200

    update stripeController.php, now is compatible with all

--- api/Controller/stripeController.php
+++ api/Controller/stripeController.php
@@ -12,9 +12,13 @@ function stripeController($uri, $apiKey)
 
 
         case 'POST':
-            //if(!isset($_POST['amount']) || empty($_POST['amount']) || !isset($_POST['name']) || empty($_POST['name'])) {
-            //    exit_with_message("Error, amount and name are mandatory to pay");
-            //}
+
+            $body = file_get_contents("php://input");
+            $json = json_decode($body, true);
+
+            if(!isset($json['amount']) || empty($json['amount']) || !isset($json['name']) || empty($json['name'])) {
+                exit_with_message("Error, amount and name are mandatory to pay");
+            }
             $tmp_array = ["payment", "subscription"];
 
             if(!in_array($uri[3], $tmp_array)) {
@@ -22,8 +26,8 @@ function stripeController($uri, $apiKey)
             }
 
             $stripeService = new StripeService();
-            //$stripeService->startPayment($_POST['amount'], $_POST['name'], $_POST['type']);
-            $stripeService->startPayment(12, "test", "payment");
+            $stripeService->startPayment($json['amount'], $json['name'], $uri[3]);
+            //$stripeService->startPayment(12, "test", $uri[3]);
             break;
 
         case 'PUT':
