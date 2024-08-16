7b41b39dec44d78cc442bee84045e8a4b4d58019
Spatulox <M78stormtrooper@laposte.net>
2024-08-16 17:09:56 +0200

    update premium.php, add stripe part to pay

--- front/HTML/premium.php
+++ front/HTML/premium.php
@@ -1,7 +1,8 @@
 <?php include("../includes/loadLang.php");?>
 
+
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
 
     <?php include("../includes/head.php"); ?>
@@ -12,37 +13,28 @@
 
 <?php include("../includes/header.php");?>
 
-<main>
-
-    <section class="flex flexCenter wrap">
-        <h1 class="width100 textCenter noMarginBottom"><?php echo($data["template"]["sectionTitle"]) ?></h1>
-    </section>
-
-    <section>
-        <div class="premiumBox">
-            Premium 1 Month
-            prod_QflheD8zyM1zCN
-            id=1
+<main style="background-color: #e1edf9;">
+    <div class="containerPremium">
+        <div class="product-box" onclick="payerPremium(event)">
+            <h2>Premium 1 Month</h2>
+            <p>Description du produit premium 1.</p>
+            <input type="hidden" name="id" value="price_1PoQ9wFP4zc2O5WMdRpyuFaq">
+            <input type="hidden" name="time" value="1">
         </div>
-        <div class="premiumBox">
-            Premium 3 Month
-            prod_Qflh88TLcaQ7rM
-            id=3
+        <div class="product-box" onclick="payerPremium(event)">
+            <h2>Premium 3 Month</h2>
+            <p>Description du produit premium 2.</p>
+            <input type="hidden" name="id" value="price_1PoQAHFP4zc2O5WMetg7EbRx">
+            <input type="hidden" name="time" value="3">
         </div>
-        <div class="premiumBox">
-            Premium 1 Year
-            price_1PoQAfFP4zc2O5WMWbSl8bPa
-            id=12
+        <div class="product-box" onclick="payerPremium(event)">
+            <h2>Premium 1 Year</h2>
+            <p>Description du produit premium 3.</p>
+            <input type="hidden" name="id" value="price_1PoQAfFP4zc2O5WMWbSl8bPa">
+            <input type="hidden" name="time" value="12">
         </div>
-    </section>
-
-
-    <!-- Template de comment utiliser le module stripe créé -->
-    <input type="button" value="Payer sur Stripe" onclick="payer()">
-
+    </div>
     <script src="https://js.stripe.com/v3/" data-js-isolation="on"></script>
-
-
 
 </main>
 
@@ -50,6 +42,61 @@
 
 </body>
 </html>
+
+<script type="text/javascript">
+
+    function payerPremium(event){
+
+
+        const htmlString = `<h1>Nous vous remercions pour votre confiance</h1>
+        <h2>Vous avez souscrit a loffre "${event.target.children[0].innerHTML}"</h2>
+        <p>Chaque ${new Date().getDay()} du mois tous les ${event.target.children[3].value} mois votre premium vous sera facture</p>`
+        
+        const stripe = new GestionStripe()
+        stripe.startStripeSubscriptionUseThisOne([event.target.children[2].value], [event.target.children[0].innerHTML], {"subject":event.target.children[0].innerHTML, "htmlString":htmlString}, "moncompte.php")
+    }
+</script>
+
+<style>
+
+    .containerPremium {
+        width: 90vw;
+        margin: auto;
+        display: flex;
+        justify-content: space-evenly;
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        /*animation: fadeIn 1s forwards;*/
+    }
+
+    .product-box {
+        background-color: white;
+        border-radius: 15px; /* Coins arrondis */
+        padding: 20px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); /* Ombre légère */
+        transition: transform 0.3s ease, box-shadow 0.3s ease; /* Transition pour l'animation */
+    }
+
+    .product-box:hover {
+        transform: scale(1.05); /* Grossissement au survol */
+        box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2); /* Ombre plus prononcée au survol */
+        cursor: pointer;
+    }
+
+    .product-box > h2, .product-box > p{
+        pointer-events: none;
+    }
+
+    @keyframes fadeIn {
+        to {
+            opacity: 1; /* Rendre visible */
+        }
+    }
+
+</style>
 
 
 
