243ea9ec7e9d66df1b367ede01d7b8793869db20
Spatulox <M78stormtrooper@laposte.net>
2024-08-17 17:23:25 +0200

    same for the login

--- api/Repository/loginRepository.php
+++ api/Repository/loginRepository.php
@@ -20,7 +20,8 @@ class LoginRepository {
         	exit_with_message("Wrong username or password", 500);
         }
 
-        return new UserModel($user[0]['id_user'], $user[0]['nom'], $user[0]['prenom'], $user[0]['date_inscription'], $user[0]['email'], $user[0]['prenom'], $user[0]['telephone'], $user[0]['id_role'], $user[0]['apikey'], 3, 1, $user[0]['date_premium'], $user[0]['month_premium']);
+        return returnUser($user, "yep@yep.com");
+        //return new UserModel($user[0]['id_user'], $user[0]['nom'], $user[0]['prenom'], $user[0]['date_inscription'], $user[0]['email'], $user[0]['prenom'], $user[0]['telephone'], $user[0]['id_role'], $user[0]['apikey'], 3, 1, $user[0]['date_premium'], $user[0]['month_premium']);
     }   
 
 }
