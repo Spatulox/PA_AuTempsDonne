e806506bc5ea1e6e50603c63bb0020d7d1bbb64c
Spatulox <M78stormtrooper@laposte.net>
2024-08-19 17:30:43 +0200

    update vehiculeModel, add new fields

--- api/Models/vehiculeModel.php
+++ api/Models/vehiculeModel.php
@@ -7,14 +7,20 @@ class VehiculeModel
     public $nom_du_vehicules;
     public $nombre_de_place;
     public $id_entrepot;
+    public $appartenance;
+    public $id_service;
+    public $immatriculation;
 
-    public function __construct($id_vehicule, $capacite, $nom_du_vehicules, $nombre_de_place, $id_entrepot)
+    public function __construct($id_vehicule, $capacite, $nom_du_vehicules, $nombre_de_place, $id_entrepot, $appartenance, $id_service, $immatriculation)
     {
         $this->id_vehicule = $id_vehicule;
         $this->capacite = $capacite;
         $this->nom_du_vehicules = $nom_du_vehicules;
         $this->nombre_de_place = $nombre_de_place;
         $this->id_entrepot = $id_entrepot;
+        $this->appartenance = $appartenance;
+        $this->id_service = $id_service;
+        $this->immatriculation = $immatriculation;
     }
 }
 
