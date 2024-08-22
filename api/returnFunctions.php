5fad5e0c4392c0f7d4580b2d150be746a84e4dc3
Spatulox <M78stormtrooper@laposte.net>
2024-08-22 16:40:49 +0200

    update returnFunctions, now keys match the DB

--- api/returnFunctions.php
+++ api/returnFunctions.php
@@ -5,11 +5,11 @@ function returnUser($dataFromDb, $address = "yep@yep.com", $id = 0){
     return $user;
 }
 function returnVehicle($dataFromDb, $id = 0){
-    $vehicle = new VehiculeModel($dataFromDb[$id]["id_vehicule"], $dataFromDb[$id]["capacite"], $dataFromDb[$id]["nom_du_vehicules"], $dataFromDb[$id]["nombre_de_place"], $dataFromDb[$id]["id_entrepot"], $dataFromDb[$id]["appartenance"], $dataFromDb[$id]["immatriculation"]);
+    $vehicle = new VehiculeModel($dataFromDb[$id]["id_vehicule"], $dataFromDb[$id]["capacite"], $dataFromDb[$id]["nom_du_vehicules"], $dataFromDb[$id]["nombre_de_place"], $dataFromDb[$id]["id_ventrepot"], $dataFromDb[$id]["appartenance"], $dataFromDb[$id]["immatriculation"], $dataFromDb[$id]["id_owner"]);
     return $vehicle;
 }
 
 function returnService($dataFromDb, $id = 0){
-    $service = new ServiceModel($dataFromDb[$id]["id_service"], $dataFromDb[$id]["description"], $dataFromDb[$id]["type_service"], $dataFromDb[$id]["date_debut"], $dataFromDb[$id]["date_fin"], $dataFromDb[$id]["id_user"]);
+    $service = new ServiceModel($dataFromDb[$id]["id_service"], $dataFromDb[$id]["description_service"], $dataFromDb[$id]["type_service"], $dataFromDb[$id]["service_date_debut"], $dataFromDb[$id]["service_date_fin"]);
     return $service;
 }
