99af93b577cb3dfdb9ffd38b65110d1448b4da57
Spatulox <M78stormtrooper@laposte.net>
2024-08-22 16:41:09 +0200

    update ServiceModel, add 'addUser' function

--- api/Models/serviceModel.php
+++ api/Models/serviceModel.php
@@ -7,15 +7,18 @@ class ServiceModel
     public $type_service;
     public $date_debut;
     public $date_fin;
-    public $id_user;
+    public $user;
 
-    public function __construct($id_service, $description, $type_service, $date_debut, $date_fin, $id_user)
+    public function __construct($id_service, $description, $type_service, $date_debut, $date_fin)
     {
         $this->id_service = $id_service;
         $this->description = $description;
         $this->type_service = $type_service;
         $this->date_debut = $date_debut;
         $this->date_fin = $date_fin;
-        $this->id_user = $id_user;
+    }
+
+    public function addUser(UserModel $user){
+        $this->user = $user;
     }
 }
