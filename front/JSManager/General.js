eadff0c9f29495c264c9a2b9327e3c24488a6988
Spatulox <M78stormtrooper@laposte.net>
2024-08-27 16:34:41 +0200

    update General.js, fix lang crash (I hope)

--- front/JSManager/General.js
+++ front/JSManager/General.js
@@ -2,7 +2,7 @@ class General {
 
     constructor() {
         this.lang = getCookie("lang")
-        this.msg = dico[this.lang]
+        this.msg = (dico[this.lang] !== undefined ? dico[this.lang] : dico["EN"])
         this.frontaddresse = ipAddressFront;
     }
 
@@ -32,7 +32,7 @@ class General {
             return message
         } else {
             const text = await response.json()
-            // DEBUG alertDebug()
+            // DEBUG alertDebug() (a commenter) ou mettre un console.log
             if(showMessage === true){
                 alert(`Impossible de réaliser cette requête (${response.statusText}) : ${response.url}`)
             }
