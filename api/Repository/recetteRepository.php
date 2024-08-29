2c92de2ef5cae0141f66dda4721410a9e6028d6a
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-08-29 09:53:10 +0200

    create recetteRepo and recetteService

---
+++ api/Repository/recetteRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once './Repository/BDD.php';
+include_once './exceptions.php';
+
+class recetteRepository
+{
+    function __construct()
+    {
+
+    }
+}
+?>
---
+++ api/Service/recetteService.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once './Repository/recetteRepository.php';
+include_once './index.php';
+
+class recetteService {
+
+
+}
+?>
