a40ea47afeb7f9d301be21d0574dda421f8848ed
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-08-29 11:44:22 +0200

    Update recettemodel

--- api/Models/recetteModel.php
+++ api/Models/recetteModel.php
@@ -3,20 +3,18 @@
 include_once './Repository/BDD.php';
 
 class recetteModel {
-    
+
     public $id_recette;
     public $nom_recette;
     public $description_recette;
-    public $quantite_recette;
-    public $unit_mesure_ingredient;
+    public $liste;
 
 
-    public function __construct($id_recette, $nom_recette,$description_recette,$quantite_recette,$unit_mesure_ingredient) {
+    public function __construct($id_recette, $nom_recette,$description_recette,$liste) {
         $this->id_recette = $id_recette;
         $this->nom_recette = $nom_recette;
         $this->description_recette = $description_recette;
-        $this->quantite_recette = $quantite_recette;
-        $this->unit_mesure_ingredient = $unit_mesure_ingredient;
+        $this->liste = $liste;
     }
 
 }
