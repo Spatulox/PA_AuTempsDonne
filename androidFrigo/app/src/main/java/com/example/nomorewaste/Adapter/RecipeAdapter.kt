b50daa4a584c8146550bc71bef938923db1c3edd
Spatulox <M78stormtrooper@laposte.net>
2024-08-30 23:34:10 +0200

    update RecipeAdapter to have click function on a element

--- androidFrigo/app/src/main/java/com/example/nomorewaste/Adapter/RecipeAdapter.kt
+++ androidFrigo/app/src/main/java/com/example/nomorewaste/Adapter/RecipeAdapter.kt
@@ -1,14 +1,22 @@
 package com.example.nomorewaste.Adapter
 
+//import RecipeDetailsActivity
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.nomorewaste.BleuBleuBleu
 import com.example.nomorewaste.Models.Recipe
 import com.example.nomorewaste.R
 
-class RecipeAdapter(private val recipes: List<Recipe>) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+
+class RecipeAdapter(
+    private val context: Context,
+    private val recipes: List<Recipe>,
+) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
 
     class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val recipeName: TextView = itemView.findViewById(R.id.recipeName)
@@ -24,7 +32,15 @@ class RecipeAdapter(private val recipes: List<Recipe>) : RecyclerView.Adapter<Re
         val recipe = recipes[position]
         holder.recipeName.text = recipe.nom_recette
         holder.ingredientsCount.text = "${recipe.nb_ingredients_matches} ingrédients correspondants"
+
+        holder.itemView.setOnClickListener {
+            // Ouvre une activity avec l'ID_recette
+            val intent = Intent(context, BleuBleuBleu::class.java)
+            intent.putExtra("RECIPE_ID", recipe.id_recette)
+            context.startActivity(intent)
+        }
     }
+
 
     override fun getItemCount() = recipes.size
 }
