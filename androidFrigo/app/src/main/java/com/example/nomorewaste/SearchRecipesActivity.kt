2b7ea6e101d62c915760a50bf006b2eaed3fdd68
Spatulox <M78stormtrooper@laposte.net>
2024-08-30 17:56:23 +0200

    add SearchRecipesActivity
    Did nothing

---
+++ androidFrigo/app/src/main/java/com/example/nomorewaste/SearchRecipesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nomorewaste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchRecipesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_recipes)
+    }
+}
