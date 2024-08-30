621963366cad23a8d73e08787758078cbeebfd9a
Spatulox <M78stormtrooper@laposte.net>
2024-08-30 23:35:25 +0200

    update RecipeApi()
    Add getRecipeById

--- androidFrigo/app/src/main/java/com/example/nomorewaste/api/endpoint/RecipeApi.kt
+++ androidFrigo/app/src/main/java/com/example/nomorewaste/api/endpoint/RecipeApi.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.util.Log
 import com.example.nomorewaste.Models.Ingredient
 import com.example.nomorewaste.Models.Recipe
+import com.example.nomorewaste.Models.RecipeDetail
 import com.example.nomorewaste.Models.RecipeSearchRequest
 import com.example.nomorewaste.api.RequestApi
 import com.example.nomorewaste.utils.Popup
@@ -17,7 +18,8 @@ class RecipeApi() {
     var popup = Popup()
 
     companion object {
-        const val ENDPOINT = "/recette/search"
+        const val ENDPOINT = "/recette"
+        const val ENDPOINTSEARCH = "/recette/search"
     }
 
     suspend fun getRecipe(context: Context, dataList: RecipeSearchRequest?): List<Recipe>? {
@@ -26,7 +28,7 @@ class RecipeApi() {
 
             val requestData = dataList?.let { convertToMap(it) } ?: emptyMap()
 
-            RequestApi().Post(context, ENDPOINT, requestData) { data ->
+            RequestApi().Post(context, ENDPOINTSEARCH, requestData) { data ->
                 Log.d("RecipeApi", "Raw response: $data")
                 if (data is JSONArray) {
                     val recipeList = mutableListOf<Recipe>()
@@ -54,6 +56,49 @@ class RecipeApi() {
             }
         }
     }
+
+    suspend fun getRecipeId(context: Context, id: Int): RecipeDetail? {
+        return suspendCoroutine { continuation ->
+            RequestApi().Get(context, "$ENDPOINT/$id") { data ->
+                Log.d("RecipeApi", "Raw response: $data")
+                if (data is JSONArray) {
+                    try {
+                        val jsonObject = data.getJSONObject(0)
+                        Log.d("RecipeApi", "Parsing recipe: $jsonObject")
+
+                        val idRecette = jsonObject.getInt("id_recette")
+                        val nomRecette = jsonObject.getString("nom_recette")
+                        val descriptionRecette = jsonObject.getString("description_recette")
+
+                        // Analyser la liste des ingrédients
+                        val ingredientsList = mutableListOf<Ingredient>()
+                        val listeIngredients = jsonObject.getJSONArray("liste")
+                        for (i in 0 until listeIngredients.length()) {
+                            val ingredientObject = listeIngredients.getJSONObject(i)
+                            val idIngredient = ingredientObject.getInt("id_ingredient")
+                            val nameIngredient = ingredientObject.getString("nom_ingredient")
+                            val quantiteRecette = ingredientObject.getString("quantite_recette")
+                            val unitMesureIngredient = ingredientObject.getString("unit_mesure_ingredient")
+
+                            // Créer un objet Ingredient et l'ajouter à la liste
+                            val ingredient = Ingredient(idIngredient, nameIngredient, quantiteRecette.toInt(), unitMesureIngredient)
+                            ingredientsList.add(ingredient)
+                        }
+
+                        // Créer et retourner l'objet Recipe
+                        continuation.resume(RecipeDetail(idRecette, nomRecette, descriptionRecette, ingredientsList))
+                    } catch (e: Exception) {
+                        Log.e("RecipeApi", "Error parsing recipe", e)
+                        continuation.resume(null)
+                    }
+                } else {
+                    Log.e("RecipeApi", "Unexpected response type: ${data?.javaClass?.simpleName}")
+                    continuation.resume(null)
+                }
+            }
+        }
+    }
+
 
     private fun convertToMap(request: RecipeSearchRequest): Map<String, Any> {
         val ingredientsList = request.ingredients?.map { ingredient ->
