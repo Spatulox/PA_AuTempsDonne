2b9ce3c39f950f91453a0c3a474d9415bef5a850
Spatulox <M78stormtrooper@laposte.net>
2024-08-30 23:36:24 +0200

    update SearchRecipeViewModel, add data class RecipeDetails

--- androidFrigo/app/src/main/java/com/example/nomorewaste/Models/SearchedRecipeViewModel.kt
+++ androidFrigo/app/src/main/java/com/example/nomorewaste/Models/SearchedRecipeViewModel.kt
@@ -6,3 +6,10 @@ data class Recipe(
     val nb_ingredients_matches: Int,
     val pourcentage_ingredients: Int
 )
+
+data class RecipeDetail(
+    val id_recette: Int,
+    val nom_recette: String,
+    val description: String,
+    val ingredient: List<Ingredient>
+)
